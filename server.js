c9ea2f63d4c5bfc1c7240df32f4015b3c446dc99
Amit Kumar Singh <amit.ks30999@gmail.com>
2018-12-27 00:25:23 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,45 @@
+var express=require('express');
+var app=express();
+var hbs=require('hbs');
+hbs.registerPartials(__dirname+"/views/partials");
+var fs=require('fs');
+app.set("view engine","hbs");
+app.use(express.static(__dirname+"/public"));
+hbs.registerHelper("getCurrentYear",()=>{
+  return new Date().getFullYear();
+})
+
+hbs.registerHelper("screamer",(text)=>{
+  return text.toUpperCase();
+})
+
+
+app.use((req,res,next)=>{
+  var now=new Date().toString();
+  var log=`${now},${req.method},${req.url}`;
+  console.log(log);
+  fs.appendFile("server.log",log +'\n',(err)=>{
+    if(err){
+      console.log("Error in appending file");
+    }
+  });
+   next();
+})
+
+app.use((req,res,next)=>{
+  console.log("Going to maintanence page");
+  res.render("maintainance");
+})
+app.get("/",(req,res)=>{
+  res.render("home",{
+    title:"Home Page",
+    message:"Welcome My Friend"
+  })
+})
+
+app.get("/about",(req,res)=>{
+  res.render("about",{title:"About Page"});
+})
+app.listen(4000,()=>{
+  console.log("App is running");
+})
